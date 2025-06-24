fd0c88f0cda911677976ae192441270e97633664
nathan-wallace <nathan.s.wallace@gmail.com>
2025-06-24 17:04:29 -0400

    Remove unused Inter font setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Document() {
   return (
